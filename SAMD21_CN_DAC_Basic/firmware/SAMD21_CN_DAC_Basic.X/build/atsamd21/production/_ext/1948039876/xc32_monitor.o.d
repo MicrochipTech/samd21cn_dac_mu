a112bb1c73348e94d19563006a927306830ed6e2
caffeiNxed <133094911+caffeiNxed@users.noreply.github.com>
2023-09-01 18:26:57 +0800

    Add files via upload

---
+++ SAMD21_CN_DAC_Basic/firmware/SAMD21_CN_DAC_Basic.X/build/atsamd21/production/_ext/1948039876/xc32_monitor.o.d
@@ -0,0 +1,2 @@
+build/atsamd21/production/_ext/1948039876/xc32_monitor.o: \
+ ../src/config/atsamd21/stdio/xc32_monitor.c
---
+++ SAMD21_CN_DAC_Basic/firmware/SAMD21_CN_DAC_Basic.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# atsamd21 configuration
+CND_ARTIFACT_DIR_atsamd21=dist/atsamd21/production
+CND_ARTIFACT_NAME_atsamd21=SAMD21_CN_DAC_Basic.X.production.hex
+CND_ARTIFACT_PATH_atsamd21=dist/atsamd21/production/SAMD21_CN_DAC_Basic.X.production.hex
